076298310a78cc765e61e82e3cf00d523add5c50
jbw9964 <jbw9964@gmail.com>
2024-09-07 16:23:41 +0900

    feat: ProductService 구현
    - 간단한 CRUD 서비스 메서드 구현

---
+++ Application/src/main/java/practice/application/services/ProductService.java
@@ -0,0 +1,51 @@
+package practice.application.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import practice.application.models.entities.ProductEntity;
+import practice.application.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepo;
+
+    @Autowired
+    public ProductService(ProductRepository productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public List<ProductEntity> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public ProductEntity getProductById(UUID id) {
+        return productRepo.findById(id)
+                          .orElse(null);
+    }
+
+    public ProductEntity saveProduct(ProductEntity product) {
+        assert product != null;
+
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(UUID id) {
+        assert id != null;
+
+        productRepo.deleteById(id);
+    }
+
+    public ProductEntity updateProduct(ProductEntity product) {
+        assert product != null;
+        assert product.getProductId() != null;
+
+        return productRepo.save(product);
+    }
+
+    public long countProducts() {
+        return productRepo.count();
+    }
+}
