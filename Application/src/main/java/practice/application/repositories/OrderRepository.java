a9e7c94a17668d5e1836f92e7f8db4f061bc38fa
jbw9964 <jbw9964@gmail.com>
2024-09-08 22:08:07 +0900

    feat: 매일 14 시에 배송하는 기능 구현

--- Application/src/main/java/practice/application/repositories/OrderRepository.java
+++ Application/src/main/java/practice/application/repositories/OrderRepository.java
@@ -1,10 +1,16 @@
 package practice.application.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import practice.application.models.entities.OrderEntity;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {}
+public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
+
+    @Query("SELECT o FROM OrderEntity o LEFT JOIN FETCH o.orderItems WHERE o.orderStatus = :orderStatus")
+    List<OrderEntity> findAllByOrderStatus(String orderStatus);
+}
 
 // TODO 추후 문서화 필요
