b8e05cbb4bd36bcb1e5d583bc2e03795822503a2
JeongIna <jeongina89@gmail.com>
2024-09-09 13:19:53 +0900

    init:프로젝트 초기 설정

--- Application/src/main/java/practice/application/Application.java
+++ Application/src/main/java/practice/application/Application.java
@@ -2,10 +2,11 @@ package practice.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //스케줄링을 위한 애노테이션
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
