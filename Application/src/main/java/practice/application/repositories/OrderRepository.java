7fffa3e125f0a6d82e39955cfe9b234113446cc9
JeongIna <jeongina89@gmail.com>
2024-09-09 13:43:59 +0900

    feat:OrderRepository에 findOrdersByDateRange() 추가

--- Application/src/main/java/practice/application/repositories/OrderRepository.java
+++ Application/src/main/java/practice/application/repositories/OrderRepository.java
@@ -1,9 +1,12 @@
 package practice.application.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import practice.application.models.entity.OrderEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,4 +14,7 @@ import java.util.UUID;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
     List<OrderEntity> findByEmail(String email);
+    @Query("SELECT o FROM OrderEntity o WHERE o.createdAt BETWEEN :startOfDay AND :endOfDay")
+    List<OrderEntity> findOrdersByDateRange(@Param("startOfDay") LocalDateTime startOfDay, @Param("endOfDay") LocalDateTime endOfDay);
+
 }
