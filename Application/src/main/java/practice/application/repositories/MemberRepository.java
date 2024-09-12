448c1e4c8cf31beff8df0dd25fed47ddc7ee0787
ihun-il <hunil9978@gmail.com>
2024-09-12 13:57:42 +0900

    fix : OrderStatus RESERVED를 Enum의 전체 패키지 경로로 변경

--- Application/src/main/java/practice/application/repositories/MemberRepository.java
+++ Application/src/main/java/practice/application/repositories/MemberRepository.java
@@ -1,6 +1,8 @@
 package practice.application.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import practice.application.models.MemberEntity;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Boolean existsByEmail(String email);
 
     Optional<MemberEntity> findByEmail(String email);
+
+    @Query("select distinct m from MemberEntity m left join fetch m.orderEntityList oe where m.id = :memberId")
+    Optional<MemberEntity> findFetchById(@Param("memberId") Long memberId);
 }
